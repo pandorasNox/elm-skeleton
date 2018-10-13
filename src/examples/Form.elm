53459a40860352fa8c17cf80f5b8209ca941b752
pandorasNox <itspandorasnox@gmail.com>
2018-10-13 19:05:31 +0200

    add example Form

---
+++ src/examples/Form.elm
@@ -0,0 +1,90 @@
+module Main exposing (Model, Msg(..), init, main, update, view, viewInput, viewValidation)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , age : Int
+    }
+
+
+init : Model
+init =
+    Model "" "" "" 0
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+    | Age String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Name name ->
+            { model | name = name }
+
+        Password password ->
+            { model | password = password }
+
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
+        Age age ->
+            { model | age = (String.toInt age |> Maybe.withDefault 0) }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewInput "number" "How old are you?" (String.fromInt model.age) Age
+        , viewValidation model
+        ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    if String.length model.password < 8 then
+        div [ style "color" "red" ] [ text "password to short" ]
+
+    else if model.password == model.passwordAgain then
+        div [ style "color" "green" ] [ text "OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Passwords do not match!" ]
